b9e7a89fcce5e4e535a97b8a255ba90cb3260d37
jean <jean.chambras@epfl.ch>
2019-11-07 19:01:20 +0100

    Fix implementation bug in encoder

--- project_code/src/Encoder.java
+++ project_code/src/Encoder.java
@@ -9,11 +9,24 @@ public class Encoder {
 
     public byte[] encode (Message m ) {
         byte[] b = new byte[9];
-        ProcessDetails send = m.getSender();
-        ProcessDetails source = m.getSource();
-        String payload = m.getPayload();
-
         b[0] = 0;
+        b[1] = IntToByteArray(m.getDestination().getId())[1];
+        b[2] = IntToByteArray(m.getDestination().getId())[0];
+        b[3] = IntToByteArray(m.getSource().getId())[1];
+        b[4] = IntToByteArray(m.getSource().getId())[0];
+        b[5] = IntToByteArray(m.getSender().getId())[1];
+        b[6] = IntToByteArray(m.getSender().getId())[0];
+        b[7] = IntToByteArray(Integer.parseInt(m.getPayload()))[1];
+        b[8] = IntToByteArray(Integer.parseInt(m.getPayload()))[0];
+
+        return b;
+    }
+
+    public byte[] encode (Ack a ) {
+        Message m = a.getMessage();
+        byte[] b = new byte[9];
+
+        b[0] = 1;
         b[1] = IntToByteArray(m.getDestination().getId())[1];
         b[2] = IntToByteArray(m.getDestination().getId())[0];
         b[3] = IntToByteArray(m.getSource().getId())[1];
@@ -35,18 +48,18 @@ public class Encoder {
     }
      */
 
-    public Tuple<Integer, Message> decode (Byte[] b){
+    public Tuple<Integer, Message> decode (byte[] b){
         int ack = ByteArrayToInt(new byte[] {b[0]});
         return new Tuple<>(ack, getMessage(b));
     }
 
 
     //todo checker l'ordre
-    private Message getMessage(Byte[] b) {
-        ProcessDetails desti = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[1], b[2]}));
-        ProcessDetails source = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[3], b[4]}));
-        String payload = Integer.toString(ByteArrayToInt(new byte[] {b[5], b[6]}));
-        ProcessDetails sender = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[7], b[8]}));
+    private Message getMessage(byte[] b) {
+        ProcessDetails desti = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[2], b[1]}));
+        ProcessDetails source = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[4], b[3]}));
+        String payload = Integer.toString(ByteArrayToInt(new byte[] {b[6], b[5]}));
+        ProcessDetails sender = nT.getProcessFromId(ByteArrayToInt(new byte[] {b[8], b[7]}));
         return new Message(desti, source, payload, sender);
     }
 
