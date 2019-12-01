fc5f8b8051c789681cf3472156d09063a4ebe851
jean <jean.chambras@epfl.ch>
2019-12-01 18:41:28 +0100

    Improve parser lisibility

--- project_code/src/Da_proc.java
+++ project_code/src/Da_proc.java
@@ -1,57 +1,69 @@
 import java.io.*;
 import java.util.*;
 
-/** Da_proc is the main process, the application starts here. It reads the membership file and creates a new Process
+/**
+ * Da_proc is the main process, the application starts here. It reads the membership file and creates a new Process
  */
 
 public class Da_proc {
     public static void main(String[] args) {
 
-        String thisLine;
-
-        if(args.length != 3)
-        {
+        // Check number of arguments
+        if (args.length != 3) {
             System.out.println("Wrong number of arguments");
-            System.exit(0);
+            System.exit(-1);
         }
 
+        // parse arguments
         int processToLaunch = Integer.parseInt(args[0]);
+        String membershipFilePath = args[1];
+        int numberOfMessages = Integer.parseInt(args[2]);
+
+
+        // start to parse membership file
+        String line;
         try {
             // open input stream membership file
-            BufferedReader br = new BufferedReader(new FileReader(args[1]));
+            BufferedReader br = new BufferedReader(new FileReader(membershipFilePath));
             // Read the first line of the membership file
-            int numberProc = 0;
-            if((thisLine = br.readLine()) != null) {
-                numberProc = Integer.parseInt(thisLine);
+            int numberOfProcesses = 0;
+            if ((line = br.readLine()) != null) {
+                numberOfProcesses = Integer.parseInt(line);
             }
 
             ArrayList<ProcessDetails> processesInNetwork = new ArrayList<>();
-            for (int i =0; i < numberProc;++i){
-                thisLine = br.readLine();
-                String[] process_details = thisLine.split("\\s+");
-                processesInNetwork.add(new ProcessDetails(Integer.parseInt(process_details[0]),process_details[1],Integer.parseInt(process_details[2])));
+            for (int i = 0; i < numberOfProcesses; ++i) {
+                line = br.readLine();
+                String[] process_details = line.split("\\s+");
+
+                int id = Integer.parseInt(process_details[0]);
+                String address = process_details[1];
+                int port = Integer.parseInt(process_details[2]);
+
+                processesInNetwork.add(new ProcessDetails(id, address, port));
             }
 
-
             HashSet<ProcessDetails> causality = new HashSet<>();
-            for (int i =1; i <= numberProc;++i){
-                thisLine = br.readLine();
-                if(i == processToLaunch){
-                    String[] causality_informations = thisLine.split("\\s+");
-                    for(int j = 1; j < causality_informations.length; ++j){
-                        causality.add(processesInNetwork.get(Integer.parseInt(causality_informations[j])-1));
+            for (int i = 1; i <= numberOfProcesses; ++i) {
+                line = br.readLine();
+                if (i == processToLaunch) {
+                    String[] causality_informations = line.split("\\s+");
+                    // The first element in the causality informations list is the id of the process
+                    // (not to include in the causality list as each process has a causal relationship with itself)
+                    for (int j = 1; j < causality_informations.length; ++j) {
+                        causality.add(processesInNetwork.get(Integer.parseInt(causality_informations[j]) - 1));
                     }
-                    System.out.println(i + "|" + Arrays.toString(causality_informations) + " | " + causality.size());
                 }
             }
 
             ProcessDetails processToLaunchDetails = processesInNetwork.get(processToLaunch - 1);
-            Process process = new Process(processToLaunchDetails.getPort(),processToLaunch, processesInNetwork,Integer.parseInt(args[2]),causality);
+            new Process(processToLaunchDetails.getPort(), processToLaunch, processesInNetwork, numberOfMessages, causality);
 
-
-
-        } catch(Exception e) {
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
